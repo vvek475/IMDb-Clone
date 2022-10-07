f0125696ce2532aa08c4bb82b8f98f24907fcf99
vivek <vickfreeindia@gmail.com>
2022-10-08 00:15:27 +0530

    requirements

--- IMDb_clone/settings.py
+++ IMDb_clone/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = eval(env('DEBUG'))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','imdb-clone-production.up.railway.app']
 
 
 # Application definition
